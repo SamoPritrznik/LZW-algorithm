acc7f3a7ded96354035b7a885ab615e2560b311a
SamoPritrznik <pritrznik.samo@gmail.com>
2022-03-22 10:49:23 +0100

    creating the first version of the encoding

--- naloga2.py
+++ naloga2.py
@@ -1,3 +1,5 @@
+
+
 def naloga2(vhod: list, nacin: int) -> tuple[list, float]:
     """
     Izvedemo kodiranje ali dekodiranje z algoritmom LZW.
@@ -25,6 +27,32 @@ def naloga2(vhod: list, nacin: int) -> tuple[list, float]:
             Kompresijsko razmerje
     """
 
+    encode(vhod)
     izhod = []
     R = float('nan')
     return (izhod, R)
+
+def encode(vhod):
+    izhod = []
+    dict = {i: chr(i) for i in range(256)}
+    N = ''
+    i = 256
+    for z in vhod:
+        if((N+z) in dict): N = N+z
+        else:
+            izhod.append(ord(N))
+            dict.update(i,(N+z))
+            N = z
+            i = i + 1
+    return izhod
+
+naloga2([
+        "B",
+        "R",
+        "B",
+        "R",
+        "R",
+        "R",
+        "R",
+        "R"
+    ], 0)
